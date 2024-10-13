658f4313d4590ef8a7b082f5c87713ab02b406e8
vishalajairosen <vishalajairosen@gmail.com>
2024-10-14 01:29:46 +0530

    Exception Updated

--- src/logger.py
+++ src/logger.py
@@ -2,11 +2,12 @@ import logging
 import os
 from datetime import datetime
 
-LOG_FILE=f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
-logs_path=os.path.join(os.getcwd(),"logs",LOG_FILE)
-os.makedirs(logs_path,exist_ok=True)
 
-LOG_FILE_PATH=os.path.join(logs_path,LOG_FILE)
+log_dir = os.path.join(os.getcwd(), "logs")
+os.makedirs(log_dir, exist_ok=True)
+
+LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+LOG_FILE_PATH = os.path.join(log_dir, LOG_FILE)
 
 
 logging.basicConfig(
